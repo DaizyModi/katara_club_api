913b076efb8c6813804e125cbd503a634a5f8757
Jigar Tarpara <jigartarpara68@gmail.com>
2020-12-02 08:43:50 +0530

    Forget Password API

--- katara_club_api/apis/auth.py
+++ katara_club_api/apis/auth.py
@@ -2,7 +2,8 @@ import frappe
 from frappe.website.utils import is_signup_enabled
 from frappe.utils import escape_html
 from frappe import throw, msgprint, _
-
+import pyotp
+from frappe.core.doctype.sms_settings.sms_settings import send_sms
 @frappe.whitelist(allow_guest=True)
 def login(usr,pwd):
 	"""
@@ -77,3 +78,49 @@ def user_sign_up(email, first_name, last_name, gender, dob, qatar_id, mobile_no,
 def update_password(pwd):
 	frappe.db.set_value("User",frappe.session.user,"new_password",pwd)
 	return 1,_("Success")
+
+@frappe.whitelist(allow_guest=True)
+def forget_password(mobile_no):
+	if not check_if_usr_registered(mobile_no):
+		totp = pyotp.TOTP('base32secret3232')
+		otp = totp.now()
+		frappe.cache().set(str(mobile_no)+'--otp_sent',otp)
+		frappe.cache().expire(str(mobile_no)+'--otp_sent', 180)
+		msg = 'Your verification code is '+otp
+		
+		try:
+			send_sms(mobile_no,msg)
+		except:
+			return {"success_key":0, "message":"Something is wrong with your mobile number."}
+		return {"success_key":1, "message":"Verification code is sent to phone number."}
+	else:
+		return {"success_key":0, "message":"User with this mobile number is Already registered."}
+@frappe.whitelist()
+def verify_otp(mobile_no,otp, password):
+	if frappe.request.method == 'POST':
+		verify = frappe.safe_decode(frappe.cache().get(str(mobile_no)+'--otp_sent'))
+		if verify is None:
+			return {
+			"success_key":0,
+			"message":"Invalid verification code"
+			}
+		check = int(verify) == int(otp)
+		if check:
+			get_user = frappe.get_doc("User", frappe.session.user)
+			get_user.mobile_no = mobile_no
+			get_user.new_password = password
+			get_user.save(ignore_permissions=True)
+			return {"success_key":1,"message":"Your OTP is validated.Password Updated"}
+		else:
+			return {
+			"success_key":2,
+			"message":"OTP expired. Please resend otp."
+			}
+	else:
+		return {"success_key":0, "message":"Method Not Allowed"}
+def check_if_usr_registered(phone):
+	user = frappe.db.get("User", {"mobile_no": phone})
+	if user:
+		return True
+	else:
+		return False
